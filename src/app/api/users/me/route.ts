5b0524ab0e222af2cdd737d58fbd4c4726fb745a
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-02-04 13:45:09 +0530

    added middleware and added me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+import { connect } from "@/db/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect()
+
+export async function GET(request: NextRequest){
+    try{
+        const userID = await getDataFromToken(request)
+        const user = await User.findOne({_id: userID}).select("-password")
+
+        return NextResponse.json({
+            message: "User Found",
+            data: user
+        })
+    }
+    catch(error: any){
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 export default function profile() {
-    return(
-        <div className="flex flex-col justify-center items-center min-h-screen">
-            <h1 className="text-4xl">Profile</h1>
-        </div>
-    )
+  const router = useRouter();
+  const [data, setData] = useState("nothing")
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id)
+  };
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-4xl">Profile</h1>
+      <h2>{data === 'nothing'? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+
+      <button className="bg-yellow-400 px-2 py-2 rounded-lg mb-2" onClick={getUserDetails}>
+        getUserDetails
+      </button>
+
+      <button className="bg-green-400 px-2 py-2 rounded-lg" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  );
 }
